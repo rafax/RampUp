e783a4aff2895841bfbae7cfc81ca1a9b78010e3
Scooletz <scooletz@gmail.com>
2016-02-06 15:58:10 +0100

    Initial draft of a scheduler, providing a way of introducing cross-cutting components. The Scheduler is a separate actor which handles messages, providing some contract for a scheduling actor. It's cohosted with a scheduling actor by using CompositeActor. It handles timeout messages issuing proper methods on the other actor. The idea is that any crosscutting service will consist of a cohosted actor responding to some internal messages calling the actor using the the service

---
+++ src/RampUp.Tests/Actors/GuidMessageWriter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using RampUp.Actors;
+using RampUp.Actors.Impl;
+using RampUp.Buffers;
+
+namespace RampUp.Tests.Actors
+{
+    public sealed class GuidMessageWriter : IMessageWriter
+    {
+        public const int MessageId = 9873845;
+        public const int ChunkLength = 16;
+
+        public unsafe bool Write<TMessage>(ref Envelope envelope, ref TMessage message, WriteDelegate write)
+            where TMessage : struct
+        {
+            if (typeof (TMessage) != typeof (Guid))
+            {
+                throw new ArgumentException("Guids only!");
+            }
+
+            var guid = (Guid) ((object) message);
+
+            var ch1 = new ByteChunk((byte*) &guid, 8);
+            var ch2 = new ByteChunk(((byte*) &guid) + 8, 8);
+            return write(MessageId, ch1, ch2);
+        }
+    }
+}
